836bed65c1d4c7e53b1ce9c465d27df086f96c7e
joethorley <joe@poissonconsulting.ca>
2020-04-28 16:55:28 -0700

    add biosite data

---
+++ R/biosite.R
@@ -0,0 +1,11 @@
+#' Biosite Data
+#'
+#' A tibble of the different sites
+#'
+#' @format A tibble with the columns:
+#' \describe{
+#'   \item{BioSite}{Name of the site.}
+#'   \item{Waterbody}{Name of the waterbody.}
+#'   \item{geometry}{Location of the site including elevation.}
+#' }
+"biosite"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(magrittr)
+
 rm(list = ls())
 
 conn <- poissqlite::ps_connect_sqlite("~/Poisson/Databases/curtis-qua-prod.sqlite")
@@ -7,3 +10,10 @@ poissqlite::ps_load_tables(conn = conn, rename = stringr::str_to_lower)
 poissqlite::ps_disconnect_sqlite(conn)
 
 usethis::use_data(analyte, overwrite = TRUE)
+
+biosite %<>% tibble::as_tibble()
+
+biosite %<>% mutate(BioSite = factor(BioSite),
+                    Waterbody = factor(Waterbody, levels = unique(Waterbody)))
+
+usethis::use_data(biosite, overwrite = TRUE)
